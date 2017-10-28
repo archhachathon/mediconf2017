ee36690173d74dc436f23dab9e54d8dc8a6a918f
Marat Monnié <maratmonnie@marats-mbp.dhcp.wustl.edu>
2017-10-28 13:59:59 -0500

    server cleaned of non used route

--- js/app2.js
+++ js/app2.js
@@ -3,17 +3,14 @@ var app = express();
 var path = require('path');
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
-});
+});*/
 
 app.get('/dashboard', function(req, res) {
     res.sendFile(path.join(__dirname + '/../html/dashboard.html'));
 });
 
-/*app.get('/video', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../html/video.html'));
-});*/
 
 app.listen(3000, function(){
 	console.info('Server listening on port ' + 3000);
