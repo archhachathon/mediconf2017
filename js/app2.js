321c5aabd0f2874792212610d00240ee56e7baab
marattm <mtulepbayevmonnie@hawk.iit.edu>
2017-10-28 15:56:04 -0500

    Revert "added a javascript version of sendSMS ajax request"
    This reverts commit 16e7d08b526e43bb4c06216129960df5f2e51ba1.

--- js/app2.js
+++ js/app2.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-<<<<<<< HEAD
 var accountSid = 'AC2f877c67f02edcbe8479a8fa21b89253'; // Your Account SID from www.twilio.com/console
 var authToken = '115e66de11ace97dfa3d083ff4980d34';   // Your Auth Token from www.twilio.com/console
 
@@ -10,12 +9,15 @@ var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
 // viewed at http://localhost:3000
+<<<<<<< HEAD
 =======
 // viewed at http://localhost:8080
 <<<<<<< HEAD
 >>>>>>> parent of ee36690... server cleaned of non used route
 =======
 >>>>>>> parent of ee36690... server cleaned of non used route
+=======
+>>>>>>> parent of 16e7d08... added a javascript version of sendSMS ajax request
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });
@@ -24,10 +26,13 @@ app.get('/dashboard', function(req, res) {
     res.sendFile(path.join(__dirname + '/../html/dashboard.html'));
 });
 
+<<<<<<< HEAD
 /*app.get('/video', function(req, res) {
     res.sendFile(path.join(__dirname + '/../html/video.html'));
 });*/
 <<<<<<< HEAD
+=======
+>>>>>>> parent of 16e7d08... added a javascript version of sendSMS ajax request
 
 app.listen(3000, function(){
 	console.info('Server listening on port ' + 3000);
