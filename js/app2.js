23ceca2ea6d320d50bbc661d409cd5ca2aa69ead
Marat Monnié <maratmonnie@marats-mbp.dhcp.wustl.edu>
2017-10-28 14:00:47 -0500

    minor changes

--- js/app2.js
+++ js/app2.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:3000
 /*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });*/
